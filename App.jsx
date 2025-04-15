aea38b59af433b25423f9598598fb005923550ee
Scarlet Fate <michaelemediong13@gmail.com>
2025-04-15 16:31:43 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Landing from './component/landing'
+
+
+const App = () => {
+  return (
+    <div>
+      <Landing />
+      
+    </div>
+  )
+}
+
+export default App
